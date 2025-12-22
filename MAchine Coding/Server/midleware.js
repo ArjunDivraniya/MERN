c2ac768f1b2dd0bbd99b2a9396d91a39b160b15b
Arjun Divraniya <arjun.divraniya.cg@email.com>
2025-12-22 23:08:10 +0530

    Add GET route for welcome message in middleware

--- MAchine Coding/Server/midleware.js
+++ MAchine Coding/Server/midleware.js
@@ -6,6 +6,10 @@ app.use((req, res, next) =>{
 console.log(req.method , req.url ,new Date())
 next();
 })
+app.get('/',(req,res , next) => {
+    res.json({message: "Welcome to Middleware Server"})
+    next();
+})
 
 app.listen(3000, () => {
     console.log("Middleware server is running on port 3000");
